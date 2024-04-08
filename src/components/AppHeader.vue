d98154f61b6e14a45841d22be07cf76677304466
simone-nolberto <simone.nolberto@outlook.com>
2024-04-08 15:41:03 +0200

    update with header creation

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,7 +6,7 @@ export default {
         return {
             socialsIcon: ["fa-brands fa-square-facebook", "fa-brands fa-square-pinterest", "<fa-brands fa-square-x-twitter", "fa-brands fa-linkedin", "<fa-brands fa-square-instagram"],
 
-            sections: [],
+            sections: ['Home', 'About', 'Services', 'Training', 'Blogs'],
         }
     }
 }
@@ -16,23 +16,37 @@ export default {
 <template>
 
     <div class="top_header">
-        <div class="contacts">
-            <ul>
+        <div class="container">
+
+            <ul class="contacts">
                 <li><i class="fa-solid fa-envelope"></i><a href="mailto:#">info@DogCare.com</a></li>
                 <li><i class="fa-solid fa-phone"></i><a href="tel:+">1(817)9013377</a></li>
             </ul>
-        </div>
-        <div class="socials">
-            <ul>
+
+            <ul class="socials">
                 <li v-for="icon in socialsIcon"><i :class="icon"></i></li>
             </ul>
+
         </div>
+
     </div>
 
     <div class="nav">
-        <div class="logo"></div>
-        <div class="menu"></div>
+        <div class="container">
+            <div class="logo">
+                <img src="../assets/img/logo.png" alt="">
+            </div>
+            <div>
+                <ul class="menu">
+                    <li v-for="section in sections">{{ section }}</li>
+                    <a href="#">Contattaci!</a>
+                </ul>
+            </div>
+        </div>
+
     </div>
+
+
 
 
 </template>
